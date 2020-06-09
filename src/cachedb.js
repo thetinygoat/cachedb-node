60565bbac8f3f6e071a1ba7dc54a3184dbb788f0
yash <shyash222@gmail.com>
2020-06-09 22:55:27 +0530

    add basic api

--- src/cachedb.js
+++ src/cachedb.js
@@ -1,11 +1,19 @@
 const axios = require('axios');
+const Sets = require('./sets');
+const Lists = require('./lists');
 
 class Cachedb {
-  constructor(port, user, password) {
-    this.port = port;
-    this.user = user;
-    this.password = password;
-    this.baseURL = `http://localhost:${this.port}`;
-  }
+	constructor(port, user, password) {
+		this.port = port;
+		this.user = user;
+		this.password = password;
+		this.baseURL = `http://localhost:${this.port}`;
+		this.headers = {
+			'X-Cachedb-User': this.user,
+			'X-Cachedb-Password': this.password,
+		};
+		this.sets = new Sets(this.baseURL, this.headers);
+		this.lists = new Lists(this.baseURL, this.headers);
+	}
 }
 module.exports = Cachedb;
--- src/sets.js
+++ src/sets.js
@@ -1,13 +1,56 @@
 const axios = require('axios');
 
 class Sets {
-	constructor(baseURL) {
+	constructor(baseURL, headers) {
 		this.baseURL = `${baseURL}/sets`;
+		this.config = {
+			baseURL: this.baseURL,
+			headers,
+		};
 	}
 
-	async set() {
+	async get(key) {
 		try {
-			const response = await axios.post(`${this.baseURL}/set`);
+			if (!key) throw 'No key provided';
+			const response = await axios({
+				url: `/get/${key}`,
+				method: 'GET',
+				...this.config,
+			});
+
+			return response.data === '<nil>' ? null : response.data;
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
+
+	async set(key, value = '', ttl = -1) {
+		try {
+			if (!key) throw 'No key provided';
+			ttl = +ttl == NaN ? -1 : ttl; //if ttl is not a number change it to max ttl
+			const response = await axios({
+				url: `/set/${key}`,
+				method: 'POST',
+				params: {
+					value,
+					ttl,
+				},
+				...this.config,
+			});
+			return response.data;
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
+
+	async del(key) {
+		try {
+			if (!key) throw 'No key provided';
+			const response = await axios({
+				url: `/del/${key}`,
+				method: 'POST',
+				...this.config,
+			});
 			return response.data;
 		} catch (e) {
 			throw new Error(e);
